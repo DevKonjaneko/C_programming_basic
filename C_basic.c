43379cbbe63fc209cf2d63e4a721f2e0abe898e4
DevKonjaneko <devfromheaven.2023@gmail.com>
2025-11-14 11:14:46 +0700

    added printf, scanf

--- C_basic.c
+++ C_basic.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+//Global Variable
+char player_name [20];
 
 int main(){
+    //Local Variable
+
+    printf("Enter Your Name: ");
+    scanf ("%s", player_name);
+    int player_hp = 100;
+    int player_maxhp = 100;
+    
+    printf("Name: %s Hp: %d/%d", player_name, player_hp, player_maxhp);
 
     return 0;
 }
+
+/* 
+int main():
+int  = return type
+main = function
+return 0;
+คืนค่า 0 ให้ระบบปฏิบัติการแสดงว่าโปรแกรมทำงานเสร็จแล้ว
+*/
